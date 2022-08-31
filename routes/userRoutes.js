8e902881dbad4fddb693fb6f53ba222655f0026e
joaooviedo <joaovitorgiroldi@gmail.com>
2022-08-31 00:01:07 -0300

    feat: factories

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,14 +1,23 @@
 export class UserRoutes {
-    constructor(controller, router) {
-      this.userController = controller;
-      this.route = router;
-    }
-  
-    route() {
-      router.get("/", this.userController.find);
-      router.post("/create", this.userController.create);
-      router.patch("/update/:id", this.userController.update);
-      router.delete("/delete/:id", this.userController.delete);
-      router.get("/:id", this.userController.findById);
-    }
+  constructor(controller, router) {
+    this.userController = controller;
+    this.router = router;
   }
+
+  route() {
+    this.router.get("/", (req, res) => this.userController.find(req, res));
+    this.router.post("/create", (req, res) =>
+      this.userController.create(req, res)
+    );
+    this.router.patch("/update/:id", (req, res) =>
+      this.userController.update(req, res)
+    );
+    this.router.delete("/delete/:id", (req, res) =>
+      this.userController.delete(req, res)
+    );
+    this.router.get("/:id", (req, res) =>
+      this.userController.findById(req, res)
+    );
+    return this.router;
+  }
+}
